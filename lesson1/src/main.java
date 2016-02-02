dd8e72bef8fcdb93eb2a7b9f052fc350f2ff65ef
DesiHume <desi_hume@ex.ua>
2016-02-02 12:19:08 +0200

    git

---
+++ lesson1/src/main.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Ром4н on 12.01.2016.
+ */
+public class main {
+
+    public static void main (String args[])
+    {
+        int [] a;
+        a = new int[10];
+        int a=4, b=1, s=1;
+        System.out.println ("Эта Ваша первая программа на Java");
+        System.out.println(a*b);
+        if  s<11 {
+        a[s]=b;
+        a[s=1]=b+1;
+        System.out.println(a[s]);
+
+
+    }
+}
